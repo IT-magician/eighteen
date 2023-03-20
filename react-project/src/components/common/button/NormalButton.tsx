60966ef8cd1fe998f1d85fed61e95dd3c6f27908
한유경 <ygo65312@naver.com>
2023-03-20 11:11:59 +0900

    add : NormalButton 컴포넌트 추가

--- react-project/src/components/common/button/NormalButton.tsx
+++ react-project/src/components/common/button/NormalButton.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+type Colortype = "blue" | "red" | "gradation";
+
 interface Props {
   text: string;
-  color: string;
+  color: Colortype;
   onClick: () => void;
 }
 
@@ -18,6 +20,54 @@ const NormalButton = ({ text, color, onClick }: Props): JSX.Element => {
   );
 };
 
-const StyledButton = styled.button<{ color: string }>``;
+const StyledButton = styled.button<{ color: Colortype }>`
+  font: inherit;
+  font-weight: 900;
+  color: var(--black-50);
+  padding: 16px 24px;
+  border: 0;
+  border-radius: 16px;
+  background-color: ${({ color }) => {
+    switch (color) {
+      case "red":
+        return "var(--pink-500)";
+      case "gradation":
+        return "var(--gradation)";
+      case "blue":
+      default:
+        return "var(--blue-500)";
+    }
+  }};
+  box-shadow: var(--shadow);
+
+  &:hover {
+    background-color: ${({ color }) => {
+      switch (color) {
+        case "red":
+          return "var(--pink-400)";
+        case "gradation":
+          return "var(--gradation)";
+        case "blue":
+        default:
+          return "var(--blue-400)";
+      }
+    }};
+  }
+
+  &:active {
+    background-color: ${({ color }) => {
+      switch (color) {
+        case "red":
+          return "var(--pink-600)";
+        case "gradation":
+          return "var(--gradation)";
+        case "blue":
+        default:
+          return "var(--blue-600)";
+      }
+    }};
+    box-shadow: none;
+  }
+`;
 
 export default NormalButton;
