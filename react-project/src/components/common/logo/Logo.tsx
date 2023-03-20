c038830ef9248a8f23c5757723c362363364ab62
한유경 <ygo65312@naver.com>
2023-03-20 12:50:47 +0900

    add : 로고 컴포넌트 생성

---
+++ react-project/src/components/common/logo/Logo.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+/**
+ * 로고 컴포넌트
+ * 클릭 시 홈화면으로 이동합니다.
+ */
+const Logo = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate("home");
+  };
+
+  return (
+    <StyledDiv onClick={onClick}>
+      <p>EIGHTEEN</p>
+      <img src={`${process.env.PUBLIC_URL}/eighteen-logo.png`} />
+    </StyledDiv>
+  );
+};
+
+const StyledDiv = styled.div`
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  cursor: pointer;
+  display: flex;
+  align-items: flex-end;
+  letter-spacing: 0.1em;
+
+  & > p {
+    margin: 2px 8px;
+    font-size: 14px;
+  }
+
+  & > img {
+    height: 24px;
+  }
+`;
+
+export default Logo;
