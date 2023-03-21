10e93bece3ebdbd6fc6fd2803e0a4d7bd94110ab
euntol <mokbee27@gmail.com>
2023-03-21 22:15:48 +0900

    add: src/components/mypage/Profile

---
+++ react-project/src/components/mypage/Profile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Profile = (): JSX.Element => {
+  return (
+    <StyledDiv>
+      <div></div>
+    </StyledDiv>
+  );
+};
+
+const StyledDiv = styled.div``;
+
+export default Profile;
