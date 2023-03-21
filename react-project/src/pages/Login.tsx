22f1dfcabeac478290f93523b2d1c14fbae2bcda
euntol <mokbee27@gmail.com>
2023-03-21 17:01:39 +0900

    modify: src/pages/Login

--- react-project/src/pages/Login.tsx
+++ react-project/src/pages/Login.tsx
@@ -7,13 +7,38 @@ import { KakaoLoginButton, GoogleLoginButton } from "../components/login/button"
  */
 const Login = (): JSX.Element => {
   return (
-    <StyledDiv>
+    <Container>
+      <TitleText>너도 몰랐던 너의 에이틴</TitleText>
+      <SubtitleDiv>
+        <SubtitleText>에이틴과 함께</SubtitleText>
+        <SubtitleText>신나는 노래방 라이프를 즐기러 가볼까요?</SubtitleText>
+      </SubtitleDiv>
       <KakaoLoginButton></KakaoLoginButton>
       <GoogleLoginButton></GoogleLoginButton>
-    </StyledDiv>
+    </Container>
   );
 };
 
-const StyledDiv = styled.div``;
+const Container = styled.div`
+  position: relative;
+`;
 
+const TitleText = styled.p`
+  position: absolute;
+  left: 28px;
+  top: 160px;
+  margin: 0px;
+  font-size: 32px;
+`;
+
+const SubtitleDiv = styled.div`
+  position: absolute;
+  left: 28px;
+  top: 235px;
+`;
+
+const SubtitleText = styled.p`
+  margin: 0px;
+  font-size: 24px;
+`;
 export default Login;
