cc6128b60d910635b60687be2b4bbd74deb5191e
bshello25 <bshello25@gmail.com>
2023-03-22 16:16:11 +0900

    valid check fix

--- src/main/java/com/eighteen/authservice/controller/AuthController.java
+++ src/main/java/com/eighteen/authservice/controller/AuthController.java
@@ -3,9 +3,10 @@ package com.eighteen.authservice.controller;
 import com.eighteen.authservice.jwt.AuthToken;
 import com.eighteen.authservice.oauth.PrincipalDetails;
 import com.eighteen.authservice.oauth.PrincipalOauth2UserService;
+import com.eighteen.authservice.repository.UserRepository;
+import com.eighteen.authservice.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -14,20 +15,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ServerWebExchange;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -43,6 +36,9 @@ public class AuthController {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/success")
     public String publishToken(
@@ -80,14 +76,16 @@ public class AuthController {
             System.out.println(userId);
             System.out.println(expiration);
             if (expiration.before(new Date())) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Expired JWT token");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Expired access token");
             }
         } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid JWT token");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid access token");
         }
         if (userId == null || userId.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid JWT token");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid access token");
         }
+        User user = userRepository.findByUserId(userId);
+        if (user == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid user");
         return ResponseEntity.ok(userId);
     }
 
@@ -109,10 +107,7 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("UNAUTHORIZED");
         }
 
-        // JWT 토큰 검증을 위해 필요한 secret key 값을 가져옵니다.
         String secretKey = env.getProperty("jwt.secret");
-
-        // JWT 토큰을 파싱하고 검증합니다.
         String userId = null;
         String accessToken = null;
         try {
@@ -133,9 +128,11 @@ public class AuthController {
         if (userId == null || userId.isEmpty() || accessToken == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
         }
+        User user = userRepository.findByUserId(userId);
+        if (user == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid user");
         HttpHeaders headers = new HttpHeaders();
         headers.add("accessToken", accessToken);
-        // JWT 토큰 검증이 성공하면 userId 값을 반환합니다.
+
         return ResponseEntity.ok().headers(headers).body(userId);
     }
 
