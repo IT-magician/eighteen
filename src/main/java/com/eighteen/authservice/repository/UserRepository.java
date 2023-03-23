4bd2b0368e344ac550f1064fca6c0c9be0be08fa
wonwoong <bshello25@gmail.com>
2023-03-23 09:38:26 +0900

    [features/be/auth-service] 폴더 생성

--- src/main/java/com/eighteen/authservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eighteen.authservice.repository;
-
-import com.eighteen.authservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-    public User findByUserId(String userId);
-}
