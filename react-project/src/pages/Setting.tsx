ef336f16ce173a9bdf876fcf1e402ee00d8c9a9d
euntol <mokbee27@gmail.com>
2023-03-23 16:42:52 +0900

    modify: setting

--- react-project/src/pages/Setting.tsx
+++ react-project/src/pages/Setting.tsx
@@ -5,9 +5,26 @@ import styled from "styled-components";
  * 프로필 수정 화면
  */
 const Setting = (): JSX.Element => {
-  return <StyledDiv>setting</StyledDiv>;
+  return (
+    <StyledDiv>
+      <p>프로필 설정</p>
+    </StyledDiv>
+  );
 };
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+  max-width: 430px;
+  position: relative;
+
+  & > p {
+    position: absolute;
+    max-width: 374px;
+    width: 100%;
+    left: 28px;
+    top: 184px;
+    margin: 0px 0px 40px;
+    font-size: 32px;
+  }
+`;
 
 export default Setting;
