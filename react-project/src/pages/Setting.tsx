60748c2e301fc827efae002d9ade4e6b2394dcd4
euntol <mokbee27@gmail.com>
2023-03-23 16:29:29 +0900

    add: setting-page

---
+++ react-project/src/pages/Setting.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+/**
+ * 프로필 수정 화면
+ */
+const Setting = (): JSX.Element => {
+  return <StyledDiv>setting</StyledDiv>;
+};
+
+const StyledDiv = styled.div``;
+
+export default Setting;
