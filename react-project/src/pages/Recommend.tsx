b438fc23310bb774e7ec07a8a72ca88e858fb330
한유경 <ygo65312@naver.com>
2023-03-24 15:02:05 +0900

    update : weatherState 받아오도록 수정, Weather Interface export

--- react-project/src/pages/Recommend.tsx
+++ react-project/src/pages/Recommend.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { getWeather } from "../apis/weather";
 import { RecommendHeader, RecommendList } from "../components/Recommend";
+import { weatherState } from "../recoil/atom/weatherState";
 
 /**
  * 추천 화면
  */
 const Recommend = (): JSX.Element => {
+  const [weather, setWeather] = useRecoilState(weatherState);
+
+  useEffect(() => {
+    // 날씨 정보가 아직 없으면 데이터를 받아옵니다
+    if (!weather) getWeather().then((data) => setWeather(data));
+  }, [weather]);
+
   return (
-    <StyledDiv>
-      <RecommendHeader />
-      <RecommendList />
-    </StyledDiv>
+    <StyledMain>
+      <RecommendHeader weather={weather} />
+      <RecommendList weather={weather} />
+    </StyledMain>
   );
 };
 
-const StyledDiv = styled.div``;
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 96px 16px 24px;
+`;
 
 export default Recommend;
