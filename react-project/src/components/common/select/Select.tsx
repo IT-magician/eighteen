9a1b5d35552a987dcd09d398dd773cd15f2ad4f3
ygo65312 <ygo65312@naver.com>
2023-03-27 02:43:55 +0900

    update : ul css 수정

--- react-project/src/components/common/select/Select.tsx
+++ react-project/src/components/common/select/Select.tsx
@@ -97,7 +97,7 @@ const StyledDiv = styled.div`
   & > ul {
     margin: 0;
     padding: 0;
-    background-color: var(--black-opacity);
+    background-color: var(--black-600);
     // 스크롤 디자인 CSS
     &::-webkit-scrollbar {
       width: 4px;
