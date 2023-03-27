fbc880283d76fbc50d78fd7725cbfa240f1307b1
한유경 <ygo65312@naver.com>
2023-03-27 11:06:35 +0900

    add : 상황별 추천 selector 컴포넌트 완성

--- react-project/src/components/Recommend/RecommendSelector/RecommendSituationSelector.tsx
+++ react-project/src/components/Recommend/RecommendSelector/RecommendSituationSelector.tsx
@@ -1,11 +1,50 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { ToggleTextButton } from "../../common/button";
 
 const RecommendSituationSelector = (): JSX.Element => {
-  return <StyledDiv></StyledDiv>;
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if (!search.get("situation")) toggleSearch("0");
+  }, []);
+
+  const toggleSearch = (situation: string) => {
+    setSearch({ situation }, { replace: true });
+  };
+
+  return (
+    <StyledDiv>
+      {situdationList.map((item, index) => (
+        <ToggleTextButton
+          key={index}
+          text={item.text}
+          onClick={() => toggleSearch(item.value)}
+          active={search.get("situation") === item.value}
+        />
+      ))}
+    </StyledDiv>
+  );
 };
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+  position: absolute;
+  top: 0;
+  display: flex;
+  flex-wrap: wrap;
+  & > * {
+    margin: 0 8px 8px 0;
+  }
+`;
+
+const situdationList = [
+  { text: "회식 분위기업", value: "0" },
+  { text: "여행", value: "1" },
+  { text: "데이트", value: "2" },
+  { text: "결혼식 축가", value: "3" },
+  { text: "이별", value: "4" },
+  { text: "응원이 필요할때", value: "5" },
+];
 
 export default RecommendSituationSelector;
