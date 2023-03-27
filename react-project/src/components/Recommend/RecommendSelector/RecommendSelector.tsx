926c54077ba1517a8777c6a1d32676eda54fe007
한유경 <ygo65312@naver.com>
2023-03-27 11:06:07 +0900

    add : weather selector 추가

--- react-project/src/components/Recommend/RecommendSelector/RecommendSelector.tsx
+++ react-project/src/components/Recommend/RecommendSelector/RecommendSelector.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Weather } from "../../../recoil/atom/weatherState";
 import RecommendEmotionSelector from "./RecommendEmotionSelector";
 import RecommendRankingSelector from "./RecommendRankingSelector";
 import RecommendSituationSelector from "./RecommendSituationSelector";
+import RecommendWeatherSelector from "./RecommendWeatherSelector";
 
-const RecommendSelector = (): JSX.Element => {
+interface Props {
+  weather: Weather;
+}
+
+const RecommendSelector = ({ weather }: Props): JSX.Element => {
   const { type } = useParams();
 
   return (
     <StyledDiv>
+      {type === "weather" && <RecommendWeatherSelector weather={weather} />}
       {type === "ranking" && <RecommendRankingSelector />}
       {type === "emotion" && <RecommendEmotionSelector />}
       {type === "situation" && <RecommendSituationSelector />}
