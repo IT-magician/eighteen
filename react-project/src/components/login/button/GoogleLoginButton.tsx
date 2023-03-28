15a12edf8e6aab63f568edd8a5dbc0893d974ffa
euntol <mokbee27@gmail.com>
2023-03-28 12:56:04 +0900

    remove: warning message

--- react-project/src/components/login/button/GoogleLoginButton.tsx
+++ react-project/src/components/login/button/GoogleLoginButton.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { instance } from "../../../apis";
 
 const GoogleLoginButton = (): JSX.Element => {
   const socialSignIn = (socialType: string) => {
