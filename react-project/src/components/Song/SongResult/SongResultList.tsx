c4ab5f7ce060ab6cbad14e11bde37a051072ee00
한유경 <ygo65312@naver.com>
2023-03-28 12:44:30 +0900

    update : Song 컴포넌트 완성

--- react-project/src/components/Song/SongResult/SongResultList.tsx
+++ react-project/src/components/Song/SongResult/SongResultList.tsx
@@ -15,11 +15,13 @@ const SongResultList = (): JSX.Element => {
     <StyledDiv>
       <h2>검색 결과</h2>
       {search.loading && <SongResultLoading />}
-      {search.loading || list.length || Boolean(search.keyword) || <SongResultDefault />}
-      {search.loading || list.length || (search.keyword && <SongResultEmpty />)}
-      {list.map((item, index) => (
-        <SongItem key={index} musicId={item.musicId} title={item.title} singer={item.singer} isEighteen={false} />
-      ))}
+      {search.loading || Boolean(list.length) || Boolean(search.keyword) || <SongResultDefault />}
+      {search.loading || Boolean(list.length) || (Boolean(search.keyword) && <SongResultEmpty />)}
+      <ul>
+        {list.map((item, index) => (
+          <SongItem key={index} musicId={item.musicId} title={item.title} singer={item.singer} isEighteen={false} />
+        ))}
+      </ul>
     </StyledDiv>
   );
 };
@@ -28,6 +30,31 @@ const StyledDiv = styled.div`
   & > h2 {
     font-weight: 400;
     margin-top: 40px;
+  }
+  & > ul {
+    margin: 0;
+    padding: 0;
+    /* height: 80px; */
+    display: flex;
+    flex-direction: column;
+
+    & > *:nth-child(n) {
+      margin-top: 4px;
+      animation: identifier 1s;
+    }
+    & > *:nth-child(5n) {
+      /* margin-bottom: 32px; */
+    }
+  }
+  @keyframes identifier {
+    from {
+      opacity: 0;
+      margin-top: -8px;
+    }
+    to {
+      opacity: 1;
+      margin-top: 4px;
+    }
   }
 `;
 
--- react-project/src/components/Song/SongSearch/SongSearchComponent.tsx
+++ react-project/src/components/Song/SongSearch/SongSearchComponent.tsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import SongSearchInput from "./SongSearchInput";
 
 const SongSearchComponent = (): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const addBackground = (e: Event) => {
+      if (e.target instanceof HTMLDivElement) {
+        if (ref.current && e.target.scrollTop > 120) {
+          ref.current.classList.add("bg");
+          document.querySelector("#Page")?.removeEventListener("scroll", addBackground);
+          document.querySelector("#Page")?.addEventListener("scroll", removeBackground);
+        }
+      }
+    };
+
+    const removeBackground = (e: Event) => {
+      if (e.target instanceof HTMLDivElement) {
+        if (ref.current && e.target.scrollTop <= 120) {
+          ref.current.classList.remove("bg");
+          document.querySelector("#Page")?.removeEventListener("scroll", removeBackground);
+          document.querySelector("#Page")?.addEventListener("scroll", addBackground);
+        }
+      }
+    };
+
+    document.querySelector("#Page")?.addEventListener("scroll", addBackground);
+    return () => {
+      document.querySelector("#Page")?.removeEventListener("scroll", addBackground);
+      document.querySelector("#Page")?.removeEventListener("scroll", removeBackground);
+    };
+  }, []);
+
   return (
-    <StyledDiv>
+    <StyledDiv ref={ref}>
       <SongSearchInput />
     </StyledDiv>
   );
 };
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+  top: 80px;
+  position: sticky;
+  z-index: 1;
+
+  &.bg::after {
+    content: "";
+    background: linear-gradient(to top, transparent 0.1%, var(--black-900));
+    z-index: -1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 240px;
+    pointer-events: none;
+  }
+`;
 
 export default SongSearchComponent;
