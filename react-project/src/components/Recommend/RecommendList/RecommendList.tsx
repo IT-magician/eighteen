be9b1d23d7128c32addd355b806c7d0a19800884
한유경 <ygo65312@naver.com>
2023-03-29 14:18:35 +0900

    update : ranking page server 연결

--- react-project/src/components/Recommend/RecommendList/RecommendList.tsx
+++ react-project/src/components/Recommend/RecommendList/RecommendList.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { getEighteenRanking } from "../../../apis/recommend";
 import { Weather } from "../../../recoil/atom/weatherState";
 import SampleData from "../../../utils/sample/song.json";
-import { SongSlideList } from "../../common/song";
+import { Song, SongSlideList } from "../../common/song";
 import { RecommendSelector } from "../RecommendSelector";
 
 interface Props {
@@ -10,9 +12,33 @@ interface Props {
 }
 
 const RecommendList = ({ weather }: Props): JSX.Element => {
+  const [songList, setSongList] = useState<Song[]>([]);
+  const { type } = useParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const gender = searchParams.get("gender") ?? "M";
+    const age = searchParams.get("age") ?? "1";
+    const emotion = searchParams.get("emotion") ?? "happy";
+    const situation = searchParams.get("situation") ?? "0";
+
+    switch (type) {
+      case "ranking":
+        if (!location.search) return;
+        getEighteenRanking(Number(age), gender).then(({ data }) => setSongList(data.musicDtos));
+        return;
+      case "weather":
+      case "myEighteen":
+      case "situation":
+      case "emotion":
+      default:
+        return;
+    }
+  }, [location]);
   return (
     <StyledDiv>
-      <SongSlideList songList={SampleData.data} />
+      <SongSlideList songList={songList} />
       <RecommendSelector weather={weather} />
     </StyledDiv>
   );
