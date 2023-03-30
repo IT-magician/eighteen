211b0b710be2f1c6d95136bd4def88cb6f4aba44
euntol <mokbee27@gmail.com>
2023-03-30 14:17:47 +0900

    modify : local 테스트를 위한 설정 변경 reset

--- react-project/src/react-app-env.d.ts
+++ react-project/src/react-app-env.d.ts
@@ -5,6 +5,5 @@ declare namespace NodeJS {
     NODE_ENV: "development" | "production" | "test";
     REACT_APP_OPEN_WEATHER_API_KEY: string;
     REACT_APP_SERVER_URL: string;
-    REACT_APP_TEST_URL: string;
   }
 }
